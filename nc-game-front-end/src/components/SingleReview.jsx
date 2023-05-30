62530dc401ccc899748857db09f30d542ebb2780
alexnorniella <alexnorniella@yahoo.co.uk>
2023-05-30 12:23:27 +0100

    ticket 5 d

---
+++ nc-game-front-end/src/components/SingleReview.jsx
@@ -0,0 +1,39 @@
+import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { fetchSingleReview } from "../api";
+import { useParams } from "react-router-dom";
+
+const SingleReview = () => {
+  const { review_id } = useParams();
+
+  const [singleReview, setSingleReview] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setIsLoading(true);
+    fetchSingleReview(review_id).then((response) => {
+      console.log(response);
+      setSingleReview(response.review);
+      setIsLoading(false);
+    });
+  }, []);
+
+  return (
+    <div>
+      {isLoading ? (
+        <p>Loading Review...</p>
+      ) : (
+        <div>
+          <h2 className="SingleReviewPageTitle">{singleReview.title} </h2>
+          <h4 className="CreatedAt">{singleReview.created_at}</h4>
+
+          <h3 className="SingleReviewAuthor">{singleReview.author}</h3>
+          <img className="SingleReviewImg" 
+          src={singleReview.review_img_url} />
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default SingleReview;
